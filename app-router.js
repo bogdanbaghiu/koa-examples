c9b5b78e69237547342aa50e051947414554a1d7
Bogdan Baghiu <bogdantrabajo@gmail.com>
2021-07-09 12:46:20 +0200

    Koa.js: Router tutorial

---
+++ app-router.js
@@ -0,0 +1,31 @@
+var koa = require('koa');
+var app = new koa();
+var Router = require('koa-router');
+var router = new Router();
+
+router.get('/', async function (ctx) {
+    // You can use `await` in here
+    ctx.body = "hello world";
+    console.log("success")
+});
+
+router.get('/hello', async function (ctx) {
+    ctx.body = await getMessage();
+});
+
+function getMessage() {
+    return "Hello world 2!";
+};
+
+router.post('/hello', async function (ctx) {
+    ctx.body = postMessage();
+});
+
+function postMessage() {
+    return "You just called the post method at '/hello'!\n";
+};
+
+app.use(router.routes());
+
+app.listen(3000);
+console.log("listening on 3000");
